5d47b0c3c186df34fb889452944da25d64970c3a
Kenneth Burgos <kenneth.burgos@gmail.com>
2022-02-05 17:46:29 -0400

    Alerts

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -2,25 +2,33 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static const Color primary = Colors.indigo;
-  
+
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    // Color Primario
-    primaryColor: primary,
+      // Color Primario
+      primaryColor: primary,
 
-    // AppBar Theme
-    appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      // AppBar Theme
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
 
-    // TextBurtton
-    textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(primary: primary))
-  );
+      // TextBurtton
+      textButtonTheme:
+          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)),
+
+      // FloatingActionButton
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: primary, elevation: 5),
+
+      // ElevatedButton
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            primary: primary, shape: const StadiumBorder(), elevation: 0),
+      ));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-    // Color Primario
-    primaryColor: primary,
+      // Color Primario
+      primaryColor: primary,
 
-    // AppBar Theme
-    appBarTheme: const AppBarTheme(color: primary, elevation: 0),
-
-    scaffoldBackgroundColor: Colors.black
-  );
+      // AppBar Theme
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      scaffoldBackgroundColor: Colors.black);
 }
