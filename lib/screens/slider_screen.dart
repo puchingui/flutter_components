b0c3db851165c413600be50214ebaa5c3d5fef60
Kenneth Burgos <kenneth.burgos@gmail.com>
2022-02-13 10:27:34 -0400

    slider checkboxes and switches expanded

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -10,6 +10,7 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnable = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,30 +18,65 @@ class _SliderScreenState extends State<SliderScreen> {
       appBar: AppBar(
         title: const Text('Sliders & Checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-              min: 50,
-              max: 400,
-              value: _sliderValue,
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 50,
+            max: 400,
+            value: _sliderValue,
+            activeColor: AppTheme.primary,
+            onChanged: _sliderEnable
+                ? (value) {
+                    print(value);
+                    setState(() {
+                      _sliderValue = value;
+                    });
+                  }
+                : null,
+          ),
+          // para desabilitar el check se envia null al onchanged
+          Checkbox(
               activeColor: AppTheme.primary,
+              value: _sliderEnable,
               onChanged: (value) {
-                print(value);
-                setState(() {
-                  _sliderValue = value;
-                });
-              },
+                _sliderEnable = value ?? true;
+                setState(() {});
+              }),
+          CheckboxListTile(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar Slider'),
+              value: _sliderEnable,
+              onChanged: (value) => setState(() {
+                    _sliderEnable = value ?? true;
+                  })),
+
+          Switch(
+              value: _sliderEnable,
+              onChanged: (value) => setState(() {
+                    _sliderEnable = value;
+                  })),
+
+          SwitchListTile.adaptive(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar Slider'),
+              value: _sliderEnable,
+              onChanged: (value) => setState(() {
+                    _sliderEnable = value;
+                  })),
+
+          const AboutListTile(),
+
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: const NetworkImage(
+                    'https://cdn.pixabay.com/photo/2020/07/06/17/51/batman-5377804_960_720.png'),
+                fit: BoxFit.contain,
+                width: _sliderValue,
+              ),
             ),
-            Image(
-              image: const NetworkImage(
-                  'https://cdn.pixabay.com/photo/2020/07/06/17/51/batman-5377804_960_720.png'),
-              fit: BoxFit.contain,
-              width: _sliderValue,
-            ),
-            const SizedBox(height: 50)
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
